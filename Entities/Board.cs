c55592f888c37e9be768f76c2423c165f92805f0
rafaelgust <rafael.appdevs@gmail.com>
2023-12-24 22:19:30 -0300

    adding: board and position classes

---
+++ Entities/Board.cs
@@ -0,0 +1,11 @@
+﻿namespace xadrez_csharp_console.Entities {
+    internal class Board {
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+
+        public Board(int rows, int columns) {
+            Rows = rows;
+            Columns = columns;
+        }
+    }
+}
---
+++ Entities/Position.cs
@@ -0,0 +1,20 @@
+﻿namespace xadrez_csharp_console.Entities {
+    internal class Position {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Position(int row, int column) {
+            Row = row;
+            Column = column;
+        }
+
+        public void DefineValues(int row, int column) {
+            Row = row;
+            Column = column;
+        }
+
+        public override string ToString() {
+            return $"{Row}, {Column}";
+        }
+    }
+}
